290ae10efe30c97322257279c49e7e50d91780b8
Scott Ullrich <sullrich@gmail.com>
2011-07-08 13:31:57 -0400

    Send 2>/dev/null

--- extra/varmfs.sh
+++ extra/varmfs.sh
@@ -23,4 +23,4 @@ chmod 555 ${BASEDIR}/etc/rc.d/varmfs
 mtree -Pcp ${BASEDIR}/var > ${TMPFILE}
 mv ${TMPFILE} ${BASEDIR}/etc/mtree/var.dist
 
-chroot ${BASEDIR} pkg_info > ${BASEDIR}/pkg_info.txt
+chroot ${BASEDIR} pkg_info > ${BASEDIR}/pkg_info.txt 2>/dev/null
