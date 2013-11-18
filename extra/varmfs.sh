535b6fd47dd638b2fa9ec50ac9ab342ff825d927
Ermal Luçi <eri@pfsense.org>
2013-11-18 10:40:24 +0100

    Use the correct package command for FreeBSD 10

--- extra/varmfs.sh
+++ extra/varmfs.sh
@@ -23,4 +23,8 @@ chmod 555 ${BASEDIR}/etc/rc.d/varmfs
 mtree -Pcp ${BASEDIR}/var > ${TMPFILE}
 mv ${TMPFILE} ${BASEDIR}/etc/mtree/var.dist
 
-chroot ${BASEDIR} pkg_info > ${BASEDIR}/pkg_info.txt 2>/dev/null
+if [ $FREEBSD_VERSION -ge 8 ]; then
+    pkg -c ${BASEDIR} info > ${BASEDIR}/pkg_info.txt 2> /dev/null
+else
+    chroot ${BASEDIR} pkg_info > ${BASEDIR}/pkg_info.txt 2>/dev/null
+fi
